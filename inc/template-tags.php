5df8495cbcc507bb32abfc6c0e5f70bfa1ec86c0
Jake Spurlock <whyisjake@gmail.com>
2013-04-19 22:09:04 -0700

    Swapping an h1 for an h3

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -31,7 +31,7 @@ function hanks_content_nav( $nav_id ) {
 
 	?>
 	<nav role="navigation" id="<?php echo esc_attr( $nav_id ); ?>" class="<?php echo $nav_class; ?>">
-		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'hanks' ); ?></h1>
+		<h4 class="screen-reader-text"><?php _e( 'Post navigation', 'hanks' ); ?></h4>
 
 	<?php if ( is_single() ) : // navigation links for single posts ?>
 
